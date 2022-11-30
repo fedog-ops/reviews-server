2c4e419e79f8531c93210f0e01f5b38f12bfae34
fedog-ops <felixtmcd@outlook.com>
2022-11-30 17:12:20 +0000

    OG file

--- db/connection.js
+++ db/connection.js
@@ -14,9 +14,6 @@ const config =
   ENV === 'production'
     ? {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
     }
   :{};
   
